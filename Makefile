68eed65496bcbd0b86c7cc70081d6a3f62625c26
Yuta Aoyagi <aoyagi.y.ac@m.titech.ac.jp>
2011-07-28 17:08:04 +0900

    add documentation comment.
    add Doxygen-style comment to main.c.
    add file 'Doxyfile' to configure Doxygen.
    add how to build document to Makefile.
    add ignore directory doc to .gitignore.

--- Makefile
+++ Makefile
@@ -2,6 +2,15 @@ TARGET = fss
 MODE   = g01
 OBJS   = main.obj # $(TARGET).obj
 
+.PHONY : default build doc ALL
+
+default : build doc
+
+build : ALL
+
+doc :
+	doxygen
+
 INCPATH    = ../z_tools/guigui01/
 include ../z_tools/command.txt
 include ../z_tools/com_mak.txt
--- main.c
+++ main.c
@@ -1,5 +1,18 @@
+/**
+ * @file main.c
+ * @brief FSSのメインプログラム. the main program of FSS.
+ * @author Yuta Aoyagi
+ * @since 2011-07-28
+ */
+
 #include <guigui01.h>
 
+/**
+ * @brief メイン関数. the main function.
+ * @note
+ * 現在はただメッセージを出力し正常終了するだけの処理しかない.
+ * It just print a message and exit successfully now.
+ */
 void G01Main(void) {
 	g01_putstr0("Hello, world!\n");
 }
