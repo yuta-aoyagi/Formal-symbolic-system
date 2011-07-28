84864c6a6e8cbd1e90da78ef5456df05443742a5
Yuta Aoyagi <aoyagi.y.ac@m.titech.ac.jp>
2011-07-28 23:34:28 +0900

    just reading a syntax define file.

--- main.c
+++ main.c
@@ -10,9 +10,18 @@
 /**
  * @brief メイン関数. the main function.
  * @note
- * 現在はただメッセージを出力し正常終了するだけの処理しかない.
- * It just print a message and exit successfully now.
+ * 現在はただ入力ファイルをダンプし正常終了するだけの処理しかない.
+ * It just print the content of the input file and exit successfully now.
  */
 void G01Main(void) {
-	g01_putstr0("Hello, world!\n");
+	static const unsigned char cmdusage[] = {
+		0x86, 0x50,
+		0x00, 's', 0x0c, 6, 's', 'y', 'n', 't', 'a', 'x',
+		0x40,
+	};
+	char *b = g01_bss1a1;
+	g01_setcmdlin(cmdusage);
+	g01_getcmdlin_fopen_s_0_4(0);
+	jg01_fread0_4(2 * 1024 * 1024, b);
+	g01_putstr0(b);
 }
